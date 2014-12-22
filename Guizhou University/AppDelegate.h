85a662c14647af34c8354e32c4d2223f0349842f
allen2063 <199128927@qq.com>
2014-12-22 21:09:11 +0800

    init

---
+++ Guizhou University/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Guizhou University
+//
+//  Created by ZengYifei on 14/12/22.
+//  Copyright (c) 2014年 IOS-develop. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Guizhou UniversityTests/Guizhou_UniversityTests.m
@@ -0,0 +1,34 @@
+//
+//  Guizhou_UniversityTests.m
+//  Guizhou UniversityTests
+//
+//  Created by ZengYifei on 14/12/22.
+//  Copyright (c) 2014年 IOS-develop. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Guizhou_UniversityTests : XCTestCase
+
+@end
+
+@implementation Guizhou_UniversityTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
